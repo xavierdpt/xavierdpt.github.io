96438927740c0d14026311e2e6079c5bdc100040
xavierdpt <xavierdpt@gmail.com>
2021-12-12 18:49:50 +0100

    .

--- ocaml/devhome/lessons/ce7d422878df22b8fdbbb6caf9f779a0/main.ml
+++ ocaml/devhome/lessons/ce7d422878df22b8fdbbb6caf9f779a0/main.ml
@@ -1,33 +1,23 @@
 let () = let niterations : int = try int_of_string Sys.argv.(1) with
                 | _ -> print_endline "Invalid argument, setting number of iterations to default value 2" ; 2
         and pfe : bool = Sys.file_exists ".preferences"
+        and preferences : (string,string) Hashtbl.t = Hashtbl.create 0
         in
-        if pfe then print_endline "Preferences file found" ;
-        let name_from_preferences : string option = 
-                if pfe then
-                        let file_channel : in_channel = open_in ".preferences" in
-                        try let line : string = input_line file_channel in
-                            let eq_index : int option = String.index_from_opt line 0 '=' in
-                            match eq_index with
-                            | None -> print_endline "'=' not found, ignoring line" ; None
-                            | Some x -> let left : string = String.sub line 0 x
-                                    and right : string = String.sub line (x+1) ((String.length line) - x - 1)  in
-                                            if left = "username" then
-                                                    if (String.trim right = "") then (
-                                                        print_endline ("Empty string value, ignoring line") ;
-                                                        close_in file_channel ;
-                                                        None
-                                                    ) else Some right
-                                            else (
-                                                print_endline ("Expected key 'username', found key '"^left^"'") ;
-                                                close_in file_channel ;
-                                                None
-                                            )
+        if pfe then (
+                print_endline "Preferences file found" ;
+                let file_channel : in_channel = open_in ".preferences" in
+                try let line : string = input_line file_channel in
+                    match String.index_from_opt line 0 '=' with
+                    | None -> ()
+                    | Some x -> let left : string = String.sub line 0 x
+                            and right : string = String.sub line (x+1) ((String.length line) - x - 1)  in
+                                    if (String.trim right = "") then ()
+                                    else Hashtbl.add preferences left right ;
+                        close_in file_channel
                         with | _ -> close_in_noerr file_channel;
-                                print_endline ("Could not read preferences file") ;
-                                None
-                else None in
-        let name : string = match name_from_preferences with
+                                print_endline ("Could not read preferences file")
+        ) ; 
+        let name : string = match Hashtbl.find_opt preferences "username" with
                 | Some x -> print_endline "Using name from preferences" ; x
                 | None -> (print_endline "What is your name?" ;
                 let input : string ref = ref (Scanf.scanf "%s@\n" Fun.id)
