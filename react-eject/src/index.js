66e4516da053084cc68660dd6620e1de1e9e6f52
xavierdpt <xavierdpt@gmail.com>
2021-12-31 13:08:50 +0100

    foundation

--- react-eject/src/index.js
+++ react-eject/src/index.js
@@ -1,79 +1,91 @@
 import "./index.css";
 const anime = window.anime;
 
-const foo = (next) => {
-  const actionfn = next.next().value;
+const foo = (supplier) => {
+  const actionfn = supplier.next().value;
   if (!actionfn) {
     return;
   }
-  const action = actionfn();
+  const action = actionfn(supplier);
   if (!action) {
     return;
   }
-  if (action.action === "log") {
-    console.log(action.text);
-  } else if (action.action === "animate") {
-    const { ml, text } = action;
-    const h1 = document.createElement("h1");
-    h1.setAttribute("class", ml);
-    const span = document.createElement("span");
-    span.setAttribute("class", "text-wrapper");
-    const letters = document.createElement("span");
-    letters.setAttribute("class", "letters");
-    for (let i = 0; i < text.length; ++i) {
-      const letter = document.createElement("span");
-      letter.setAttribute("class", "letter");
-      letter.appendChild(document.createTextNode(text[i]));
-      letters.appendChild(letter);
-    }
-    span.appendChild(letters);
-    h1.appendChild(span);
-    document.getElementById("root").appendChild(h1);
-
-    anime
-      .timeline({
-        loop: false,
-        duration: 10000,
-      })
-      .add({
-        targets: "." + ml + " .letter",
-        opacity: [0, 1],
-        easing: "easeOutExpo",
-        duration: 1,
-        delay: (el, i) => 30 * (i + 1),
-      })
-      .add({
-        duration: 1000,
-        targets: "body",
-        opacity: [1, 1],
-      })
-      .add({
-        targets: "." + ml + " .letter",
-        opacity: [1, 0],
-        easing: "easeOutExpo",
-        duration: 1,
-        delay: (el, i) => 30 * (i + 1),
-        complete: () => foo(next),
-      });
-  }
+  action();
 };
 
-const idGenerator = (() => {
+const createText = (ml, text, supplier) => {
+  const h1 = document.createElement("h1");
+  h1.setAttribute("class", ml);
+  const span = document.createElement("span");
+  span.setAttribute("class", "text-wrapper");
+  const letters = document.createElement("span");
+  letters.setAttribute("class", "letters");
+  for (let i = 0; i < text.length; ++i) {
+    const letter = document.createElement("span");
+    letter.setAttribute("class", "letter");
+    letter.style.opacity = 0;
+    letter.appendChild(document.createTextNode(text[i]));
+    letters.appendChild(letter);
+  }
+  span.appendChild(letters);
+  h1.appendChild(span);
+  document.getElementById("root").appendChild(h1);
+  foo(supplier);
+};
+
+const animateAppear = (ml, supplier) => {
+  anime
+    .timeline({
+      loop: false,
+      duration: 10000,
+    })
+    .add({
+      targets: "." + ml + " .letter",
+      opacity: [0, 1],
+      easing: "easeOutExpo",
+      duration: 1,
+      delay: (el, i) => 30 * (i + 1),
+      complete: () => foo(supplier),
+    });
+};
+
+const animateDisappear = (ml, supplier) => {
+  anime
+    .timeline({
+      loop: false,
+      duration: 10000,
+    })
+    .add({
+      targets: "." + ml + " .letter",
+      opacity: [1, 0],
+      easing: "easeOutExpo",
+      duration: 1,
+      delay: (el, i) => 30 * (i + 1),
+      complete: () => foo(supplier),
+    });
+};
+
+const sleep = (duration, supplier) => {
+  setTimeout(() => foo(supplier), duration);
+};
+
+const nextId = (() => {
   let id = 0;
   return () => ++id;
 })();
 
 function* sequence() {
-  yield () => ({
-    action: "animate",
-    text: "Project management ...",
-    ml: "ml" + idGenerator(),
-  });
-  yield () => ({
-    action: "animate",
-    text: "...is not new",
-    ml: "ml" + idGenerator(),
-  });
-  yield () => ({ action: "log", text: "finished" });
+  const ml1 = "ml" + nextId();
+  const ml2 = "ml" + nextId();
+  yield (supplier) => createText(ml1, "Project management ...", supplier);
+  yield (supplier) => animateAppear(ml1, supplier);
+  yield (supplier) => sleep(1000, supplier);
+  yield (supplier) => animateDisappear(ml1, supplier);
+  yield (supplier) => sleep(1000, supplier);
+  yield (supplier) => createText(ml2, "...is not new", supplier);
+  yield (supplier) => animateAppear(ml2, supplier);
+  yield (supplier) => sleep(1000, supplier);
+  yield (supplier) => animateDisappear(ml2, supplier);
+  yield (supplier) => console.log("finished");
 }
 foo(sequence());
