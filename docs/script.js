0bf8552ccf58f15a34b6bfa1cc6a2c69e2247b28
xavierdpt <37592677+xavierdpt@users.noreply.github.com>
2024-03-11 10:56:14 +0100

    .

--- docs/script.js
+++ docs/script.js
@@ -1,4 +1,5 @@
 const links = window.xdata;
+const count = Vue.ref(1);
 const lis = [];
 for (let k in links) {
     if (k === "") {
@@ -7,7 +8,7 @@ for (let k in links) {
     const link = links[k];
     lis.push(Vue.h("li", {}, [Vue.h("a", { href: link.href }, [link.label])]));
 }
-const button = Vue.h('button',{},['Click me!']);
+const button = Vue.h('button',{},['Click me! '+count.value]);
 const ul = Vue.h('ul', {}, lis);
 const vnode = Vue.h('div',{},[button,ul]);
 Vue.createApp({ render: () => vnode }).mount('#root');
