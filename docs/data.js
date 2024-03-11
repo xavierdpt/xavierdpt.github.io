fd87316b08f10b46c43f419efddbee67e27b5acb
xavierdpt <37592677+xavierdpt@users.noreply.github.com>
2024-03-11 14:13:14 +0100

    .

--- docs/data.js
+++ docs/data.js
@@ -1,226 +1,302 @@
-window.xdata ={
-    vue:{
-        label:"Vue",
-        href:"https://vuejs.org/"
+window.xdata = {
+    vue: {
+        label: "Vue",
+        href: "https://vuejs.org/"
     },
-    "whatwg-specs":{
-        label:"WHATWG Standards",
-        href:"https://spec.whatwg.org/"
+    "whatwg-specs": {
+        label: "WHATWG Standards",
+        href: "https://spec.whatwg.org/"
     },
-    "echarts":{
+    "echarts": {
         label: "Apache ECharts",
         href: "https://echarts.apache.org/"
     },
-    "modernizr":{
+    "modernizr": {
         label: "Modernizr",
         href: "https://modernizr.com/"
     },
-    "preact":{
-        label:"Preact",
-        href:"https://preactjs.com/"
+    "preact": {
+        label: "Preact",
+        href: "https://preactjs.com/"
     },
-    "webassembly":{
-        label:"WebAssembly",
-        href:"https://webassembly.org/"
+    "webassembly": {
+        label: "WebAssembly",
+        href: "https://webassembly.org/"
     },
-    "java-spec":{
-        label:"Java Language and Virtual Machine Specifications",
-        href:"https://docs.oracle.com/javase/specs/"
+    "java-spec": {
+        label: "Java Language and Virtual Machine Specifications",
+        href: "https://docs.oracle.com/javase/specs/"
     },
-    "gimple":{
-        label:"GIMPLE",
-        href:"https://gcc.gnu.org/onlinedocs/gccint/GIMPLE.html"
+    "gimple": {
+        label: "GIMPLE",
+        href: "https://gcc.gnu.org/onlinedocs/gccint/GIMPLE.html"
     },
-    "rtl":{
-        label:"RTL",
-        href:"https://gcc.gnu.org/onlinedocs/gccint/RTL.html"
+    "rtl": {
+        label: "RTL",
+        href: "https://gcc.gnu.org/onlinedocs/gccint/RTL.html"
     },
-    "baeldung":{
-        label:"Baeldung",
-        href:"https://www.baeldung.com/"
+    "baeldung": {
+        label: "Baeldung",
+        href: "https://www.baeldung.com/"
     },
-    "redux-saga":{
-        label:"Redux-Saga",
-        href:"https://redux-saga.js.org/"
+    "redux-saga": {
+        label: "Redux-Saga",
+        href: "https://redux-saga.js.org/"
     },
-    "redux":{
-        label:"Redux",
-        href:"https://redux.js.org/"
+    "redux": {
+        label: "Redux",
+        href: "https://redux.js.org/"
     },
-    "three.js":{
-        label:"three.js",
-        href:"https://threejs.org/"
+    "three.js": {
+        label: "three.js",
+        href: "https://threejs.org/"
     },
-    "babylon.js":{
-        label:"Babylon.js",
-        href:"https://www.babylonjs.com/"
+    "babylon.js": {
+        label: "Babylon.js",
+        href: "https://www.babylonjs.com/"
     },
-    "p5.js":{
-        label:"p5.js",
-        href:"https://p5js.org/"
+    "p5.js": {
+        label: "p5.js",
+        href: "https://p5js.org/"
     },
-    "narayana":{
-        label:"Narayana",
-        href:"https://www.narayana.io/"
+    "narayana": {
+        label: "Narayana",
+        href: "https://www.narayana.io/"
     },
-    "undertow":{
-        label:"Undertow",
-        href:"https://undertow.io/"
+    "undertow": {
+        label: "Undertow",
+        href: "https://undertow.io/"
     },
-    "servicemix":{
+    "servicemix": {
         label: "Apache ServiceMix",
-        href:"https://servicemix.apache.org/"
+        href: "https://servicemix.apache.org/"
     },
-    "modelio":{
-        label:"Modelio",
-        href:"https://www.modelio.org/"
+    "modelio": {
+        label: "Modelio",
+        href: "https://www.modelio.org/"
     },
-    "openjdk":{
-        label:"OpenJDK",
-        href:"https://openjdk.org/"
+    "openjdk": {
+        label: "OpenJDK",
+        href: "https://openjdk.org/"
     },
-    "caffeine":{
-        label:"Caffeine",
-        href:"https://github.com/ben-manes/caffeine"
+    "caffeine": {
+        label: "Caffeine",
+        href: "https://github.com/ben-manes/caffeine"
     },
-    "vertx":{
-        label:"Vert.x",
-        href:"https://vertx.io/"
+    "vertx": {
+        label: "Vert.x",
+        href: "https://vertx.io/"
     },
-    "spark":{
-        label:"Apache Spark",
-        href:"https://spark.apache.org/"
+    "spark": {
+        label: "Apache Spark",
+        href: "https://spark.apache.org/"
     },
-    "spring-projects":{
-        label:"Spring Projects",
-        href:"https://spring.io/projects"
+    "spring-projects": {
+        label: "Spring Projects",
+        href: "https://spring.io/projects"
     },
-    "jakarta-ee":{
-        label:"Jakarta EE",
-        href:"https://jakarta.ee/specifications/"
+    "jakarta-ee": {
+        label: "Jakarta EE",
+        href: "https://jakarta.ee/specifications/"
     },
-    "zig":{
-        label:"Zig",
-        href:"https://ziglang.org/"
+    "zig": {
+        label: "Zig",
+        href: "https://ziglang.org/"
     },
-    "nim":{
-        label:"Nim",
-        href:"https://nim-lang.org/"
+    "nim": {
+        label: "Nim",
+        href: "https://nim-lang.org/"
     },
-    "rebol":{
-        label:"Rebol",
-        href:"http://www.rebol.com/"
+    "rebol": {
+        label: "Rebol",
+        href: "http://www.rebol.com/"
     },
-    "coq":{
-        label:"Coq",
-        href:"https://coq.inria.fr/"
+    "coq": {
+        label: "Coq",
+        href: "https://coq.inria.fr/"
     },
-    "ocaml":{
-        label:"OCaml",
-        href:"https://ocaml.org/"
+    "ocaml": {
+        label: "OCaml",
+        href: "https://ocaml.org/"
     },
-    "groovy":{
-        label:"Groovy",
-        href:"https://groovy-lang.org/"
+    "groovy": {
+        label: "Groovy",
+        href: "https://groovy-lang.org/"
     },
-    "kotlin":{
-        label:"Kotlin",
-        href:"https://kotlinlang.org/"
+    "kotlin": {
+        label: "Kotlin",
+        href: "https://kotlinlang.org/"
     },
-    "idea":{
-        label:"IntelliJ IDEA",
-        href:"https://www.jetbrains.com/idea/"
+    "idea": {
+        label: "IntelliJ IDEA",
+        href: "https://www.jetbrains.com/idea/"
     },
-    "scala":{
-        label:"Scala",
-        href:"https://www.scala-lang.org/"
+    "scala": {
+        label: "Scala",
+        href: "https://www.scala-lang.org/"
     },
-    "mathjax":{
-        label:"MathJax",
-        href:"https://www.mathjax.org/"
+    "mathjax": {
+        label: "MathJax",
+        href: "https://www.mathjax.org/"
     },
-    "marked":{
-        label:"Marked",
-        href:"https://marked.js.org/"
+    "marked": {
+        label: "Marked",
+        href: "https://marked.js.org/"
     },
-    "tcp":{
-        label:"TCP",
-        href:"https://en.wikipedia.org/wiki/Transmission_Control_Protocol"
+    "tcp": {
+        label: "TCP",
+        href: "https://en.wikipedia.org/wiki/Transmission_Control_Protocol"
     },
-    "udp":{
-        label:"UDP",
-        href:"https://en.wikipedia.org/wiki/User_Datagram_Protocol"
+    "udp": {
+        label: "UDP",
+        href: "https://en.wikipedia.org/wiki/User_Datagram_Protocol"
     },
-    "ipv4":{
-        label:"IPv4",
-        href:"https://en.wikipedia.org/wiki/Internet_Protocol_version_4"
+    "ipv4": {
+        label: "IPv4",
+        href: "https://en.wikipedia.org/wiki/Internet_Protocol_version_4"
     },
-    "ipv6":{
-        label:"IPv6",
-        href:"https://en.wikipedia.org/wiki/IPv6"
+    "ipv6": {
+        label: "IPv6",
+        href: "https://en.wikipedia.org/wiki/IPv6"
     },
-    "ethernet":{
-        label:"Ethernet",
-        href:"https://en.wikipedia.org/wiki/Ethernet"
+    "ethernet": {
+        label: "Ethernet",
+        href: "https://en.wikipedia.org/wiki/Ethernet"
     },
-    "dns":{
-        label:"DNS",
-        href:"https://en.wikipedia.org/wiki/Domain_Name_System"
+    "dns": {
+        label: "DNS",
+        href: "https://en.wikipedia.org/wiki/Domain_Name_System"
     },
-    "arp":{
-        label:"ARP",
-        href:"https://en.wikipedia.org/wiki/Address_Resolution_Protocol"
+    "arp": {
+        label: "ARP",
+        href: "https://en.wikipedia.org/wiki/Address_Resolution_Protocol"
     },
-    "tls":{
-        label:"TLS",
-        href:"https://en.wikipedia.org/wiki/Transport_Layer_Security"
+    "tls": {
+        label: "TLS",
+        href: "https://en.wikipedia.org/wiki/Transport_Layer_Security"
     },
-    "snmp":{
-        label:"SNMP",
-        href:"https://en.wikipedia.org/wiki/Simple_Network_Management_Protocol"
+    "snmp": {
+        label: "SNMP",
+        href: "https://en.wikipedia.org/wiki/Simple_Network_Management_Protocol"
     },
-    "osgi":{
-        label:"OSGi",
-        href:"https://www.osgi.org/"
+    "osgi": {
+        label: "OSGi",
+        href: "https://www.osgi.org/"
     },
-    "omg":{
-        label:"OMG",
-        href:"https://www.omg.org/"
+    "omg": {
+        label: "OMG",
+        href: "https://www.omg.org/"
     },
-    "corba":{
-        label:"CORBA",
-        href:"https://www.corba.org/"
+    "corba": {
+        label: "CORBA",
+        href: "https://www.corba.org/"
     },
-    "rust":{
-        label:"Rust",
-        href:"https://www.rust-lang.org/"
+    "rust": {
+        label: "Rust",
+        href: "https://www.rust-lang.org/"
     },
-    "crates":{
-        label:"Crates",
-        href:"https://crates.io/"
+    "crates": {
+        label: "Crates",
+        href: "https://crates.io/"
     },
-    "maven":{
-        label:"Maven",
-        href:"https://maven.apache.org/"
+    "maven": {
+        label: "Maven",
+        href: "https://maven.apache.org/"
     },
-    "gradle":{
-        label:"Gradle",
-        href:"https://gradle.org/"
+    "gradle": {
+        label: "Gradle",
+        href: "https://gradle.org/"
     },
-    "jest":{
-        label:"Jest",
-        href:"https://jestjs.io/"
+    "jest": {
+        label: "Jest",
+        href: "https://jestjs.io/"
     },
-    "webrtc":{
-        label:"WebRTC",
-        href:"https://webrtc.org/"
+    "webrtc": {
+        label: "WebRTC",
+        href: "https://webrtc.org/"
     },
     "javascript.info": {
-        label:"The Modern JavaScript Tutorial",
-        href:"https://javascript.info/"
+        label: "The Modern JavaScript Tutorial",
+        href: "https://javascript.info/"
     },
-    "craftinginterpreters.com":{
-        label:"Crafting Interpreters",
-        href:"https://craftinginterpreters.com/"
+    "craftinginterpreters.com": {
+        label: "Crafting Interpreters",
+        href: "https://craftinginterpreters.com/"
+    },
+    "vue-template-explorer": {
+        label: "Vue 3 Template Explorer",
+        href: "https://template-explorer.vuejs.org/"
+    },
+    "javascript-proxy": {
+        label: "JavaScript Proxy",
+        href: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Proxy"
+    },
+    "svelte": {
+        label: "Svelte",
+        href: "https://svelte.dev/"
+    },
+    "immutable": {
+        label: "Immutable",
+        href: "https://immutable-js.com/"
+    },
+    "immer": {
+        label: "Immer",
+        href: "https://immutable-js.com/"
+    },
+    "vue-play": {
+        label: "Vue SFC Playground",
+        href: "https://play.vuejs.org/"
+    },
+    "xstate": {
+        label: "XState",
+        href: "https://xstate.js.org/"
+    },
+    "rxjs": {
+        label: "RxJS",
+        href: "https://rxjs.dev/"
+    },
+    "vueuse": {
+        label: "VueUse",
+        href: "https://vueuse.org/"
+    },
+    "solidjs": {
+        label: "SolidJS",
+        href: "https://www.solidjs.com/"
+    },
+    "angular": {
+        label: "Angular",
+        href: "https://angular.io/"
+    },
+    "qwik": {
+        label: "Qwik",
+        href: "https://qwik.dev/"
+    },
+    "knockout": {
+        label: "Knockout",
+        href: "https://knockoutjs.com/"
+    },
+    "meteor": {
+        label: "Meteor",
+        href: "https://www.meteor.com/"
+    },
+    "mobx": {
+        label: "MobX",
+        href: "https://mobx.js.org/README.html"
+    },
+    "react": {
+        label: "React",
+        href: "https://react.dev/"
+    },
+    "typescript": {
+        label: "TypeScript",
+        href: "https://www.typescriptlang.org/"
+    },
+    "babel": {
+        label: "Babel",
+        href: "https://babeljs.io/"
+    },
+    "coffeescript": {
+        label: "CoffeeScript",
+        href: "https://coffeescript.org/"
     }
 };
